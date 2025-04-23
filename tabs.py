15f1917d9b49c895768631ddb2f8f90ef3d4cdb0
infinisean <45185805+infinisean@users.noreply.github.com>
2025-04-23 04:00:55 -0400

    tabs

---
+++ tabs.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+tab1, tab2, tab3 = st.tabs(["Tab 1", "Tab 2", "Tab 3"])
+
+with tab1:
+    st.header("Tab 1")
+    st.write("Content of Tab 1")
+    st.line_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with tab2:
+    st.header("Tab 2")
+    st.write("Content of Tab 2")
+    st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with tab3:
+    st.header("Tab 3")
+    st.write("Content of Tab 3")
+    st.area_chart({"data": [1, 5, 2, 6, 2, 1]})
