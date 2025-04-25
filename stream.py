bdb61f396ea9180e7c766530c81a4df5fab1a1c1
infinisean <45185805+infinisean@users.noreply.github.com>
2025-04-25 04:17:39 -0400

    st

--- stream.py
+++ stream.py
@@ -2,7 +2,7 @@ import sys, os
 sys.path.append('/home/netmonitor/palostream')
 import streamlit as st
 from streamlit_option_menu import option_menu
-import streamlit_keyup as st_keyup
+from streamlit_keyup import st_keyup
 from pan_functions import setup_logging, read_file, get_db_credentials, palo_gen_api_key, read_pan_api_key 
 from pan_functions import send_api_query, get_pan_connected_devices, parse_system_resources, get_active_pan
 from pan_functions import get_pan_devices, parse_element_to_dict, get_pan_ha_state, display_ha_state, display_pan_devices
