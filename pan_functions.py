c3e4e734d440e64319d2143326a0a309e8ab221a
infinisean <45185805+infinisean@users.noreply.github.com>
2025-04-30 02:26:17 -0400

    hyphen

--- pan_functions.py
+++ pan_functions.py
@@ -320,7 +320,7 @@ def get_pan_devices(active_panorama):
                     'mac_address': mac_address,
                     'sw_version': sw_version,
                     'uptime': uptime,
-                    'global_protect_client_package_version': global_protect_client_package_version
+                    'global-protect_client_package_version': global_protect_client_package_version
                 })
     else:
         logging.error(f"Failed to retrieve connected devices from {active_panorama}. Status code: {response.status_code}")
