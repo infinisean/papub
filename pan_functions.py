c2df93195af5126ab95f6d3ae4dd61da56e1a0f0
infinisean <45185805+infinisean@users.noreply.github.com>
2025-04-30 01:54:31 -0400

    ..

--- pan_functions.py
+++ pan_functions.py
@@ -312,7 +312,7 @@ def get_pan_devices(active_panorama):
                     'mac_address': mac_address,
                     'sw_version': sw_version,
                     'uptime': uptime,
-                    'global-protect-client-package-version': global_protect_client_package_version
+                    'global_protect_client_package_versionn': global_protect_client_package_version
                 })
     else:
         logging.error(f"Failed to retrieve connected devices from {active_panorama}. Status code: {response.status_code}")
